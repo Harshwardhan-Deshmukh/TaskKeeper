e93fb2a53da4f68f7aadfaff77301d414e8bb984
Harshwardhan Deshmukh <harshwardhanet7007@gmail.com>
2025-03-16 23:10:47 +0530

    removed dbml

--- TaskForge/src/main/java/com/taskforge/app/advices/ApiResponse.java
+++ TaskForge/src/main/java/com/taskforge/app/advices/ApiResponse.java
@@ -1,5 +1,6 @@
 package com.taskforge.app.advices;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,6 +8,7 @@ import java.time.LocalDateTime;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class ApiResponse<T> {
 
     private T data;
